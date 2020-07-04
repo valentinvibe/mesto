50c35ae3ed60c83cf4ff7701e55bff3113d24bb3
Evgeniy Marchenkov <s0fuck@yandex.ru>
2020-07-04 13:10:31 +0300

    style: адаптивная верстка макета

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const closeModal = document.querySelector('.modal__close-button');
-const modal = document.querySelector('.modal');
-const profileEdit = document.querySelector('.profile__edit-button');
+const closeModal = document.querySelector('.modal__close-button'),
+      modal = document.querySelector('.modal'),
+      profileEdit = document.querySelector('.profile__edit-button'),
+      profileName = document.querySelector('.profile__name'),
+      profileJob = document.querySelector('.profile__job'),
+      formNameEdit = document.querySelector('.form__input_type_name'),
+      formJobEdit = document.querySelector('.form__input_type_job'),
+      form = document.querySelector('.form');
 
 
 function ToggleModal() {
    modal.classList.toggle('modal_type_open');
+   formNameEdit.value=profileName.textContent;
+   formJobEdit.value=profileJob.textContent;
 }
 
 closeModal.addEventListener('click', ToggleModal);
 profileEdit.addEventListener('click', ToggleModal);
+
+form.addEventListener('submit', function(event) {
+   event.preventDefault();
+   profileName.textContent=formNameEdit.value;
+   profileJob.textContent=formJobEdit.value;
+   ToggleModal();
+}
+);
