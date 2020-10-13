52b8316fea948eadea14f0b84e399bf8f3958ddc
Valentin <7705678@gmail.com>
2020-10-13 09:35:23 +0300

    dev: api

--- components/userInfo.js
+++ components/userInfo.js
@@ -1,8 +1,9 @@
 export default class UserInfo {
-    constructor({ nameSelector, jobSelector, photoSelector}) {
+    constructor({ nameSelector, jobSelector, photoSelector, api}) {
         this._nameSelector = nameSelector;
         this._jobSelector = jobSelector;
         this._photoSelector = document.querySelector(photoSelector);
+        this._api = api;
 
         this._nameInput = document.querySelector(this._nameSelector);
         this._jobInput = document.querySelector(this._jobSelector);
